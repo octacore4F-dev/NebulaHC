53d13ca17f9db82b7253ce8906537787475d72e4
f1uffy <138036693+f1uffy314@users.noreply.github.com>
2023-06-28 13:19:54 -0400

    Incorporate changes from the F1uffy branch

--- NebulaHC/main.py
+++ NebulaHC/main.py
@@ -1,40 +1,41 @@
-from util.tools import  *
-from util.objects import *
-from util.routines import *
+from tools import  *
+from objects import *
+from routines import *
 
 #This file is for strategy
 
 class main(GoslingAgent):
 
     def run(agent):
-
+       #Precomputing variables
        goal_to_me = agent.me.location - agent.friend_goal.location
-       my_goal_to_ball,ball_dist = (agent.ball.location - agent.friend_goal.location).normalize(True)
-       my_dist = my_goal_to_ball - goal_to_me
-       close = (agent.me.location - agent.ball.location).magnitude() < 2000
+       my_goal_to_ball,ball_dist = (agent.ball.location - agent.friend_goal.location).magnitude()
+       my_dist_to_ball = (agent.ball.location - agent.me.location)
+       me_close = (agent.me.location - agent.ball.location).magnitude() < 2000
+       oppo_0_close = (agent.foes[0].location - agent.ball.location).magnitude() < 2000
+       oppo_1_close = (agent.foes[1].location - agent.ball.location).magnitude() < 2000
        has_boost = agent.me.boost > 30
-       me_onside = (my_dist - 200).magnitude() > ball_dist
-       oppo0_onside = abs(agent.foe_goal.location.y - agent.foes[0].location.y) - 200 > abs(agent.foe_goal.location.y - agent.ball.location.y)
-       oppo1_onside = abs(agent.foe_goal.location.y - agent.foes[0].location.y) - 200 > abs(agent.foe_goal.location.y - agent.ball.location.y)
-       my_point = agent.friend_goal.location + (my_goal_to_ball * my_dist)
+       me_onside = (agent.me.location.y < -200) if (agent.friend_goal.y < 0) else (agent.me.location.y > 200)
+       oppo_0_onside = (agent.foes[0].location.y < -200) if (agent.foe_goal.y < 0) else (agent.foes[0].location.y > 200)
+       oppo_1_onside = (agent.foes[1].location.y < -200) if (agent.foe_goal.y < 1) else (agent.foes[1].location.y > 200)
        closest_oppo = 0
-
-
        if (agent.me.location - agent.foes[1].location).magnitude() < (agent.me.location - agent.foes[0].location).magnitude():
           closest_oppo = 1
 
+        #Showing debug information
        if agent.team == 0:
            agent.debug_stack()
 
+        #Main bot logic
        return_to_goal = False
 
        if len(agent.stack) < 1:
            if agent.kickoff_flag:
                agent.push(kickoff())
-           elif (close and me_onside) or ((oppo0_onside or oppo1_onside) and me_onside):
+           elif me_close and not (oppo_0_close or oppo_1_close):
+               left_field = Vector3(4200 * -side(agent.team), agent.ball.location.y + (1000 * -side(agent.team)), 0)
+               right_field = Vector3(4200 * side(agent.team), agent.ball.location.y + (1000 * side(agent.team)), 0)
                targets = {"goal":(agent.foe_goal,agent.foe_goal), "upfield": (left_field,right_field)}
-               left_field = Vector3(4200*-side(agent.team),agent.ball.location.y + (1000*-side(agent.team)),0)
-               right_field = Vector3(4200* side(agent.team),agent.ball.location.y + (1000* side(agent.team)),0)
                shots = find_hits(agent,targets)
                if len(shots["goal"]) > 0:
                    agent.push(shots["goal"][0])
@@ -46,6 +47,7 @@ class main(GoslingAgent):
                boosts = [boost for boost in agent.boosts if boost.large and boost.active and abs(agent.friend_goal.location.y - boost.location.y) - 200 < abs(agent.friend_goal)]
                if len(boosts) > [0]:
                  for boost in boosts:
+                   closest = boost
                    if (boost.location - agent.me.location).magnitude() < (closest.location - agent.me.location).magnitude():
                        closest = boost
                    agent.push(goto_boost())
@@ -55,16 +57,12 @@ class main(GoslingAgent):
                agent.push(short_shot(agent.foe_goal.location))
 
        if return_to_goal == True:
-         relative_target = agent.friend_goal.location - agent.me.locations
+         relative_target = agent.friend_goal.location - agent.me.location
          angles = defaultPD(agent, agent.me.local(relative_target))
          defaultThrottle(agent,2300)
          agent.controller.boost = False if abs(angles[1]) > 0.5 or agent.me.airborne else agent.controller.boost
          agent.controller.handbrake = True if abs(angles[1]) > 2.8 else False
 
        if (((agent.me.location - agent.foes[0].location).magnitude() < 250) or ((agent.me.location - agent.foes[1].location).magnitude() < 250)) and ball_dist < 750:
-           agent.controller.boost
+           agent.controller.boost = True
            agent.push(goto(agent.foes[closest_oppo].location))
-           time.sleep(1.5)
-
-       if (agent.me.boost < 30) and (oppo0_onside or oppo1_onside):
-           agent.push(goto_boost())
