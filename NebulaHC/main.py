ff720abffe02a7d90edbc8b9050d8fab4bfa0759
f1uffy <138036693+f1uffy314@users.noreply.github.com>
2023-06-28 16:05:52 -0400

    Add more comments, add more awareness of teammate's position

--- NebulaHC/main.py
+++ NebulaHC/main.py
@@ -10,25 +10,33 @@ class main(GoslingAgent):
        #Precomputing variables
        goal_to_me = agent.me.location - agent.friend_goal.location
        my_goal_to_ball,ball_dist = (agent.ball.location - agent.friend_goal.location).magnitude()
-       my_dist_to_ball = (agent.ball.location - agent.me.location)
+       
+       #Checking if Nebula, their opponents, and their teammate are within 2000 UU of the ball
        me_close = (agent.me.location - agent.ball.location).magnitude() < 2000
        oppo_0_close = (agent.foes[0].location - agent.ball.location).magnitude() < 2000
        oppo_1_close = (agent.foes[1].location - agent.ball.location).magnitude() < 2000
+       teammate_close = ((agent.friends[(self.index+1)%2].location - agent.ball.location).magnitude() < 2000)
+       
        has_boost = agent.me.boost > 30
+       
+       #Checking if Nebula, their opponents, and their teammate are on their side of the pitch
        me_onside = (agent.me.location.y < -200) if (agent.friend_goal.y < 0) else (agent.me.location.y > 200)
        oppo_0_onside = (agent.foes[0].location.y < -200) if (agent.foe_goal.y < 0) else (agent.foes[0].location.y > 200)
        oppo_1_onside = (agent.foes[1].location.y < -200) if (agent.foe_goal.y < 1) else (agent.foes[1].location.y > 200)
+       teammate_onside = (agent.friends[(self.index+1)%2].location.y < -200) if (agent.friend_goal.y < 1) else (agent.friends[(self.index+1)%2].location.y > 200)
+       
+       #Finding the closest opponent
        closest_oppo = 0
        if (agent.me.location - agent.foes[1].location).magnitude() < (agent.me.location - agent.foes[0].location).magnitude():
           closest_oppo = 1
 
-        #Showing debug information
+       #Showing debug information
        if agent.team == 0:
            agent.debug_stack()
 
-        #Main bot logic
+       #Main bot logic
+       
        return_to_goal = False
-
        if len(agent.stack) < 1:
            if agent.kickoff_flag:
                agent.push(kickoff())
